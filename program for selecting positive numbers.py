94ad47fae4802838ae5e43875bf563a79530a1f4
venus-ugwu <107423098+venus-ugwu@users.noreply.github.com>
2022-07-12 23:31:40 +0400

    program for selecting only positive numbers

---
+++ program for selecting positive numbers.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 12 18:00:42 2022
+
+@author: venus
+"""
+
+print ("program that prints only positive numbers")
+
+allnum = [12,-7,5,64,-14,14,-95,3]
+
+#allnum - all numbers
+#pnum = positive numbers 
+
+for pnum in allnum:
+    if pnum > 0:
+        pass 
+        print(pnum)
